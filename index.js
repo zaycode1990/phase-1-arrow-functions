67ed9f0964d70a84d5ee09c504076a8d52c1817b
Isaiah <ijandicos90@gmail.com>
2022-03-22 14:13:40 -0400

    commit tree cca95f830d045f6b109f6e49a50b263ba3d17ac8

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (function () {
+   return 2000/100;
+  });
+
+console.log(divide())
+
+const square = (parameterX) => {return parameterX **2}
+
+(square(2))
+
+let a = 3
+let b = 4
+const add = (a,b) => {return a + b}
+
+console.log(add())
